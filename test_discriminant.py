9276614102257f6b114d2137d0dbcfeb94a55d54
vidergooold <iravidergold@yandex.ru>
2025-10-16 12:44:56 +0700

    error

--- test_discriminant.py
+++ test_discriminant.py
@@ -7,7 +7,7 @@ class TestDiscriminant(unittest.TestCase):
 
     def test_positive_discriminant(self):
         # Тест для случая, когда дискриминант больше нуля
-        self.assertEqual(calculate_discriminant(1, -3, 2), 1)
+        self.assertEqual(calculate_discriminant(1, -3, 2), 2)
 
     def test_zero_discriminant(self):
         # Тест для случая, когда дискриминант равен нулю
